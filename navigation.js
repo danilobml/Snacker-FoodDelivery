5766a1a1511e6f6c1217e3ed05328011c8e2be9b
danilobml <danbarml@gmail.com>
2023-09-03 20:39:51 +0200

    modify: navigation

--- navigation.js
+++ navigation.js
@@ -1,12 +1,14 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-const Stack = createNativeStackNavigator();
-import {View, Text} from 'react-native';
+
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import CartScreen from './screens/CartScreen';
 import OrderPreparingScreen from './screens/OrderPreparingScreen';
 import DeliveryScreen from './screens/DeliveryScreen';
+import SettingsScreen from './screens/SettingsScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
   return (
@@ -21,6 +23,7 @@ export default function Navigation() {
         <Stack.Screen name="Cart" component={CartScreen} options={{ presentation: 'modal' }} />
         <Stack.Screen name="OrderPreparing" component={OrderPreparingScreen} options={{ presentation: 'fullScreenModal'}}/>
         <Stack.Screen name="Delivery" component={DeliveryScreen} options={{ presentation: 'fullScreenModal'}}/>
+        <Stack.Screen name="Settings" component={SettingsScreen} options={{ presentation: 'fullScreenModal'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
